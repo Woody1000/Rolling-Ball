24f879e5f39fe75f1ef137dd6041771a4c13ed17
Woody1000 <yazhivunazemle@gmail.com>
2019-07-19 22:30:37 +0500

    создал игрока, платформу. Игрок может прыгать, движется слева -напрво

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D _myRigidbody;
+    private Collider2D _myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody2D>();
+
+        _myCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(_myCollider, whatIsGround);
+
+        _myRigidbody.velocity = new Vector2(moveSpeed, _myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x, jumpForce);
+            }
+        }
+    }
+}
