216c3a8bc3db7d6f09471d3463da5da41ca0cddd
Woody1000 <yazhivunazemle@gmail.com>
2019-07-21 14:26:45 +0500

    Добавил тайл платформы. Камера следует за игроком. Бесконечно появляются платформы перед игроком

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController thePlayer;
+
+    private Vector3 _lastPlayerPosition;
+    private float _distanceToMove;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    
+    void Update()
+    {
+        _distanceToMove = thePlayer.transform.position.x - _lastPlayerPosition.x;
+
+        transform.position = new Vector3(transform.position.x + _distanceToMove, transform.position.y, transform.position.z);
+
+        _lastPlayerPosition = thePlayer.transform.position;
+    }
+}
